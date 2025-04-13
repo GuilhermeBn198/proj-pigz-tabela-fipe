0498f09dcbab43ff7a1ec5dbb5c5c686114476f6
Guilherme Bernardo <bguilherme51@gmail.com>
2025-04-13 15:50:35 -0400

    chore(migration): add campo name em user no banco

---
+++ projfipe2/migrations/Version20250413191622.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250413191622 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE user ADD name VARCHAR(255) NOT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE vehicle CHANGE status status ENUM('for_sale', 'sold') NOT NULL DEFAULT 'for_sale'
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE vehicle CHANGE status status ENUM('for_sale', 'sold') DEFAULT 'for_sale' NOT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE user DROP name
+        SQL);
+    }
+}
